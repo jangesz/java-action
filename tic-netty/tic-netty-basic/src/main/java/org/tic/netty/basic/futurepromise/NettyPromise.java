43bc9b1c2bf3ca7cc0734f53c650bdffda8357ad
Jange <totoro18k@hotmail.com>
2017-09-25 11:44:02 +0800

    update

---
+++ tic-netty/tic-netty-basic/src/main/java/org/tic/netty/basic/futurepromise/NettyPromise.java
@@ -0,0 +1,20 @@
+package org.tic.netty.basic.futurepromise;
+
+import io.netty.util.concurrent.Promise;
+import org.junit.Test;
+
+/**
+ * 描述信息：//TODO
+ * Jange Gu
+ * http://jange.me
+ * http://1gb.tech
+ * 2017/9/25.
+ */
+public class NettyPromise {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
